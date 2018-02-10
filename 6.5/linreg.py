53b68a8d7f7528dfaf5ebe91b6b8f1290c18901f
alex-epp <a.epp8@share.epsb.ca>
2018-02-09 17:30:55 -0700

    Added progress bar, visual, noise

--- 6.5/linreg.py
+++ 6.5/linreg.py
@@ -1,5 +1,9 @@
+import MLP
+
+import matplotlib.pyplot as plt
 import numpy.matlib as np
-from mlp import *
+import progressbar
+
 
 def generate_data(xmin, xmax, npoints, estddev, esize, f):
     '''Generates random (x, y) ordered pairs that approximately follow a given
@@ -23,19 +27,25 @@ def generate_data(xmin, xmax, npoints, estddev, esize, f):
     ys = [f(x) + e for x, e in zip(xs, err)]
     return np.array(xs), np.array(ys)
 
+
 # Generate training data
 ndata = 100
-xs, ys = generate_data(0, 5, ndata, 0, 0, lambda x: 2*x + 4)
+xs, ys = generate_data(0, 5, ndata, 1, 1, lambda x: 2*x + 4)
 
 # Construct model
-mlp = MLP()
-mlp.add_op(MatMul(np.rand(1, 1)))
-mlp.add_op(VecAdd(np.rand(1, 1)))
-#mlp.add_op(Max0()) # Doesn't actually do anything here
+mlp = MLP.MLP()
+mlp.add_op(MLP.MatMul(np.rand(1, 1)))
+mlp.add_op(MLP.VecAdd(np.rand(1, 1)))
 
 # Train
-for _ in range(1000):
+bar = progressbar.ProgressBar()
+for _ in bar(range(100)):
     mlp.backprop(xs, ys, 0.1)
 
 print("Learned slope: {}".format(mlp.operations[0].weights[0,0]))
 print("Learned intercept: {}".format(mlp.operations[1].bias[0,0]))
+
+plt.plot(xs, ys, 'bo')
+plt.plot([0, 5], [4, 14], 'b')
+plt.plot(sorted(xs), [mlp.eval(x)[0,0] for x in sorted(xs)], 'r')
+plt.show()
