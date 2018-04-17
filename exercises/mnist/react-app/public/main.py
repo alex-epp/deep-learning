e77db3556367ae0a2230767f06ed9e767361f5fe
alex-epp <a.epp8@share.epsb.ca>
2018-04-17 08:59:02 -0600

    moved MLP folder into submodule

---
+++ exercises/mnist/react-app/public/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, send_from_directory, request
+import numpy.matlib as np
+
+import sys
+sys.path.append('backend/')
+
+import MLP
+
+
+app = Flask(__name__)
+
+filenames = ['saves/mlp{}'.format(i+1) for i in range(5)]
+ensemble = MLP.Ensemble()
+ensemble.load(open(n, 'rb') for n in filenames)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    response = np.matrix(list(float(item) for item in request.get_json().values()))
+    probs = ensemble.eval(response.T)
+    pred = np.argmax(probs)
+    return '{} ({:.0f}% certainty)'.format(pred, probs[pred, 0]*100)
+
+
+@app.route('/')
+def serve():
+    return send_from_directory('.', 'index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+    # app.run(host='0.0.0.0', use_reloader=True, port=5000, threaded=True)
